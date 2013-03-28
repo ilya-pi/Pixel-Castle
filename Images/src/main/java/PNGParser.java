6a4a2231945753d415381bc82b5c492cfb8ebb64
nakedpony <belyakov.sergey@gmail.com>
2013-03-28 19:29:30 +0100

    new cool parser

---
+++ Images/src/main/java/PNGParser.java
@@ -0,0 +1,80 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PNGParser {
+
+    public static Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    public static Configuration config;
+
+    public static class ImageData {
+        private final int height;
+        private final int width;
+        private final String name;
+        private final List<Integer> pixels;
+
+        public ImageData(int height, int width, String name, List<Integer> pixels) {
+            this.height = height;
+            this.width = width;
+            this.name = name;
+            this.pixels = pixels;
+        }
+    }
+
+    public static void main(String[] args) throws ConfigurationException, IOException {
+        config = new PropertiesConfiguration("config.properties");
+        String[] imageFolders = config.getStringArray("images.resource.folders");
+        String extension = config.getString("image.extension");
+        String outputFolder = config.getString("images.resource.output.folder");
+        for (String imageFolder : imageFolders) {
+            System.out.println("=======================================");
+            System.out.println("processing imageFolder = " + imageFolder);
+            System.out.println("=======================================");
+            File folder = new File("src/main/resources/" + imageFolder);
+            File[] listOfFiles = folder.listFiles();
+            if (listOfFiles != null) {
+                List<ImageData> imagesForJson = new ArrayList<ImageData>();
+                for (File file : listOfFiles) { //todo filter out non extension files
+                    System.out.println("processing file = " + file.getName());
+                    ImageData imageData = parseImage(file);
+                    imagesForJson.add(imageData);
+                }
+                String jsonString = gson.toJson(imagesForJson);
+                FileUtils.writeStringToFile(new File(outputFolder + "/" + imageFolder + ".json"), jsonString);
+            }
+        }
+    }
+
+    private static ImageData parseImage(File file) throws IOException {
+        String name = FilenameUtils.removeExtension(file.getName());
+        List<Integer> pixels = new ArrayList<Integer>(2000);
+        BufferedImage image = ImageIO.read(file); //todo catch exceptions?
+        int width = image.getWidth();
+        int height = image.getHeight();
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int rgb = image.getRGB(x, y);
+                int alpha = (rgb >> 24) & 0xFF;
+                int red =   (rgb >> 16) & 0xFF;
+                int green = (rgb >>  8) & 0xFF;
+                int blue =  (rgb      ) & 0xFF;
+                pixels.add(red);
+                pixels.add(green);
+                pixels.add(blue);
+                pixels.add(alpha);
+            }
+        }
+        return new ImageData(height, width, name, pixels);
+    }
+}
--- Sources/scripts/util/Image.lua
+++ Sources/scripts/util/Image.lua
@@ -4,20 +4,21 @@ dkjson = require("scripts.util.dkjson")
 
 function renderImage(xPosition, yPosition, image, game)
     local pixels = {}
-    local primitiveColorsCount = 3
+    local primitiveColorsCount = 4
     for y = 0, image.height - 1 do
         for x = 0, image.width - 1 do
             local rPosition = y * image.width * primitiveColorsCount + x * primitiveColorsCount + 1
             local r = image.pixels[rPosition]
             local g = image.pixels[rPosition + 1]
             local b = image.pixels[rPosition + 2]
-            if (r + g + b ~= 0) then
+            local a = image.pixels[rPosition + 3]
+            if (a ~= 0) then
                 local left = (xPosition + x) * game.pixel
                 local top = (yPosition + y) * game.pixel
                 local pixel = display.newRect(left, top, game.pixel, game.pixel)
                 pixel.strokeWidth = 0
-                pixel:setFillColor(r, g, b)
-                pixel:setStrokeColor(r, g, b)
+                pixel:setFillColor(r, g, b, a)
+                pixel:setStrokeColor(r, g, b, a)
                 table.insert(pixels, pixel)
             end
         end
