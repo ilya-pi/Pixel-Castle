aad90b0547450780bc823d861ada7407a238b820
nakedpony <belyakov.sergey@gmail.com>
2013-04-26 21:39:14 +0200

    fix #16

--- Sources/scripts/splash.lua
+++ Sources/scripts/splash.lua
@@ -8,7 +8,21 @@ fullSplash.alpha = 0
 function splashScreen()
 	splashGroup.alpha = 0
 	fullSplash.alpha, fullSplash.x, fullSplash.y = 1, display.contentWidth / 2, display.contentHeight / 2
-	beginText = display.newText("Tap to Begin", display.contentWidth / 2 - 50, (display.contentHeight * .75), native.systemFont, 24) 
+	beginText = display.newText("Tap to Begin", display.contentWidth / 2 - 50, (display.contentHeight * .75), "TrebuchetMS", 24)
+
+--[[
+    "Trebuchet-BoldItalic"
+    "TrebuchetMS"
+    "TrebuchetMS-Bold"
+    "TrebuchetMS-Italic"
+]]
+
+--[[
+    for i,v in ipairs(native.getFontNames()) do
+        print(i .. " " .. v)
+    end
+]]
+
 end
 
 function dismissSplashScreen()
