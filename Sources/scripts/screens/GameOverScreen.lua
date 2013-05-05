39a7b07d7251f6a97b942078336ab351cfae681b
badmotherfucker <ilya.pimenov@gmail.com>
2013-05-05 20:21:38 +0200

    Added castles to gameover screen and fixed font

--- Sources/scripts/screens/GameOverScreen.lua
+++ Sources/scripts/screens/GameOverScreen.lua
@@ -1,6 +1,7 @@
 module(..., package.seeall)
 
 local widget = require("widget")
+local imageHelper = require("scripts.util.Image")
 
 GameOverScreen = {} -- required arg: game
 
@@ -13,8 +14,8 @@ function GameOverScreen:new (o)
 end
 
 local function infoText(message, x, y, size)
-    local textShadow = display.newText(message, x + 2, y + 2, "Trebuchet MS Bold", size)
-    local text = display.newText(message, x, y, "Trebuchet MS Bold", size)
+    local textShadow = display.newText(message, x + 2, y + 2, "TrebuchetMS-Bold", size)
+    local text = display.newText(message, x, y, "TrebuchetMS-Bold", size)
     -- textShadow:setReferencePoint(display.CenterReferencePoint)
     -- text:setReferencePoint(display.CenterReferencePoint)
     textShadow:setTextColor(255, 255, 255)
@@ -27,15 +28,15 @@ function GameOverScreen:render()
     local overlay = display.newRect(0, 0, display.contentWidth, display.contentHeight)
     overlay:setFillColor(195, 214, 93, 150)
 
-    local playerTextShadow = display.newText( ".", display.contentWidth / 2 + 2, display.contentHeight / 4 + 2, "Trebuchet MS Bold", 48)
-    local playerText = display.newText( ".", display.contentWidth / 2, display.contentHeight / 4, "Trebuchet MS Bold", 48)
+    local playerTextShadow = display.newText( ".", display.contentWidth / 2 + 2, display.contentHeight / 4 + 2, "TrebuchetMS-Bold", 48)
+    local playerText = display.newText( ".", display.contentWidth / 2, display.contentHeight / 4, "TrebuchetMS-Bold", 48)
     playerTextShadow:setReferencePoint(display.CenterReferencePoint)
     playerText:setReferencePoint(display.CenterReferencePoint)
     playerTextShadow:setTextColor(255, 255, 255)
     playerText:setTextColor(37, 54, 34)
 
-    local messageTextShadow = display.newText( ".", display.contentWidth / 2 + 2, display.contentHeight / 4 + 52 + 2, "Trebuchet MS Bold", 48)
-    local messageText = display.newText( ".", display.contentWidth / 2, display.contentHeight / 4 + 52, "Trebuchet MS Bold", 48)
+    local messageTextShadow = display.newText( ".", display.contentWidth / 2 + 2, display.contentHeight / 4 + 52 + 2, "TrebuchetMS-Bold", 48)
+    local messageText = display.newText( ".", display.contentWidth / 2, display.contentHeight / 4 + 52, "TrebuchetMS-Bold", 48)
     messageTextShadow:setReferencePoint(display.CenterReferencePoint)
     messageText:setReferencePoint(display.CenterReferencePoint)
     messageTextShadow:setTextColor(255, 255, 255)
@@ -92,4 +93,9 @@ function GameOverScreen:render()
             end
     }
     playAgain.x, playAgain.y = 320, 280
+
+    local castles = imageHelper.loadImageData("data/castles.json") --todo move to static initialization
+
+    imageHelper.renderImage(5, 20, castles["castle1"], 5)
+    imageHelper.renderImage(75, 20, castles["castle2"], 5)
 end
