19e344e09ac6bacadbda000cfd48cbb0e0d9e928
badmotherfucker <ilya.pimenov@gmail.com>
2013-05-13 00:49:03 +0200

    Sync

--- Sources/scripts/GameModel.lua
+++ Sources/scripts/GameModel.lua
@@ -4,6 +4,7 @@ module(..., package.seeall)
 
 GameModel = {
     delay = 100,
+	-- pixel = 10,
 	pixel = 10,
 	cameraGoBackDelay = 2500, -- delay between stop gragging the world map and a camera to go back to it's initial place
 	groundYOffset = 3,
