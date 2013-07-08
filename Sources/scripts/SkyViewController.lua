68541b7cd5b85b4c96f1db9092d784745381a2f9
Ilya Pimenov <ilya.pimenov@gmail.com>
2013-07-08 23:12:29 +0200

    Enabled background clouds to go with the wind in each level

--- Sources/scripts/SkyViewController.lua
+++ Sources/scripts/SkyViewController.lua
@@ -1,6 +1,7 @@
 module(..., package.seeall)
 
 local imageHelper = require("scripts.util.Image")
+local Memmory = require("scripts.util.Memmory")
 
 SkyViewController = { clouds = true }
 
@@ -15,8 +16,31 @@ end
 -- physics — physics object to attach to
 -- world - display group for the whole scene
 function SkyViewController:render(group)
-	imageHelper.ourImage("images/levels/" .. game.level_map.levelName .. "/sky.png", 
-		game.level_map.levelWidth * game.pixel, 
-		game.level_map.levelHeight * game.pixel,
-		group)
+	local skyWidth = game.level_map.levelWidth * game.pixel
+	local skyHeight = game.level_map.levelHeight * game.pixel
+	local skyImage = "images/levels/" .. game.level_map.levelName .. "/sky.png"
+
+	local sky1 = imageHelper.ourImage(skyImage, skyWidth, skyHeight, group)
+	local sky2 = imageHelper.ourImage(skyImage, skyWidth, skyHeight, group)
+	sky2.x = sky1.x + skyWidth
+
+    Memmory.timerStash.skyMovementTimer = timer.performWithDelay(30, function()
+        sky1.x = sky1.x + game.wind.speed / 2
+        sky2.x = sky2.x + game.wind.speed / 2
+
+        if sky1.x <= display.screenOriginX - skyWidth then
+        	print("me1")
+            local tmp = sky1
+            sky1 = sky2
+            sky2 = tmp
+            sky2.x = sky1.x + skyWidth
+        elseif sky1.x >= display.screenOriginX then
+        	print("me2")
+            local tmp = sky1
+            sky1 = sky2
+            sky2 = tmp
+            sky1.x = sky2.x - skyWidth        	
+        end
+    end, 0)
+
 end
